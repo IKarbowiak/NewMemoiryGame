bc8d3e7ac1d17d9f447e23dbba6ac661a28bb41f
igaKarbowiak <221275@student.pwr.edu.pl>
2018-09-11 18:49:11 +0200

    Add files

--- api/game/views.py
+++ api/game/views.py
@@ -1,11 +1,21 @@
 from rest_framework import generics
 from .serializers import CardDeckSerializer
-from game.models import Deck
+from game.models import Deck, MemoCard
+import random
 
 
 class DeckList(generics.ListCreateAPIView):
     """ This class defines the create behavior of our rest api. """
-    queryset = Deck.objects.filter(theme='Dogs')[0].cards
+    decks = Deck.objects.filter(theme='Dogs')
+    queryset = random.sample(list(decks), 1)[0].cards
+    # queryset = cards | cards
+    # random.seed()
+    # cards = list(deck_cards)
+    # cards.extend(deck_cards)
+    # random.shuffle(cards)
+    # cards = [card.name for card in cards]
+    # # cards_in_row = [[card for card in cards[i:i+4]] for i in range(0, len(cards), 4)]
+    # queryset = MemoCard.objects.filter(name__in=cards)
     serializer_class = CardDeckSerializer
 
     def perform_create(self, serializer):
--- game/urls.py
+++ game/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+from django.conf.urls import url
 
 
 urlpatterns = [
     path('', views.start_site, name='start_site'),
-    path(r'dogs/', TemplateView.as_view(template_name='game/dogs_deck.html'))
+    url(r'^dogs/?', TemplateView.as_view(template_name='game/dogs_deck.html'))
     # path('dogs/', views.dogs_deck, name='dogs_deck'),
 ]
 

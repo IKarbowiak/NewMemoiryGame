6de1362b346892a28adf59a5d10e0b6bcdc9e8c1
IKarbowiak <40886528+IKarbowiak@users.noreply.github.com>
2018-09-18 14:53:55 +0200

    Add files via upload

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from api.game.views import DeckList, ThemesList
+from api.game.views import CardList
+from api.themes.views import ThemesView
 
 urlpatterns = {
-    url(r'^decks/$', DeckList.as_view(), name='deck'),
-    url(r'^themes/$', ThemesList.as_view(), name='themes')
+    url(r'^decks/$', CardList.as_view(), name='create'),
+    url(r'^themes/$', ThemesView.as_view(), name='themes')
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)

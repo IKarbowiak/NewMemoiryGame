65baaab2dabedcdcdefeb5e2c56f70467e6e05c1
IKarbowiak <40886528+IKarbowiak@users.noreply.github.com>
2018-09-25 14:56:40 +0200

    Add files via upload

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,8 @@ from api.themes.views import ThemesView
 urlpatterns = {
     url(r'^decks/$', CardList.as_view(), name='create'),
     url(r'^themes/$', ThemesView.as_view(), name='themes'),
-    url(r'^game/$', GameViewSet.as_view({'post': 'create'}), name='game')
+    url(r'^game/$', GameViewSet.as_view({'post': 'create', 'get': 'list'}), name='game'),
+    # url(r'scores/$', GameViewSet.as_view(), name='scores')
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
